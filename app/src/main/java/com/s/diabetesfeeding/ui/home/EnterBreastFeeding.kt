de053aa3dd56af2c1e6376569cb95648b9215f6d
Manish <manishchauhan7830@gmail.com>
2020-08-18 23:42:19 +0530

    All is Done Design

---
+++ app/src/main/java/com/s/diabetesfeeding/ui/home/EnterBreastFeeding.kt
@@ -0,0 +1,12 @@
+package com.s.diabetesfeeding.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.s.diabetesfeeding.R
+
+class EnterBreastFeeding : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enter_breast_feeding)
+    }
+}
