6a9c673866165c98924d989d66277054547c4acf
Manish <manishchauhan7830@gmail.com>
2020-08-23 00:05:54 +0530

    today_design

---
+++ app/src/main/java/com/s/diabetesfeeding/ui/home/BreastfeedingHistory.kt
@@ -0,0 +1,12 @@
+package com.s.diabetesfeeding.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.s.diabetesfeeding.R
+
+class BreastfeedingHistory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_breastfeeding_history)
+    }
+}
